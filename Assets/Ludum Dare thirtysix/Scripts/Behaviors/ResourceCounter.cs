9b4b242022e717b4a882b526cd79c0c484652b6a
HitchH1k3r <hitch@hitchh1k3rsguide.com>
2016-08-27 14:30:57 -0400

    Added resources.

---
+++ Assets/Ludum Dare thirtysix/Scripts/Behaviors/ResourceCounter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ResourceCounter : MonoBehaviour
+{
+
+  public Resources resourcePool;
+  public Resources.Type displayType;
+
+  private Text text;
+
+  void Awake()
+  {
+    text = GetComponent<Text>();
+  }
+
+  void Update()
+  {
+    text.text = resourcePool.GetValue(displayType).ToString();
+  }
+
+}
---
+++ Assets/Ludum Dare thirtysix/Scripts/Behaviors/Resources.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resources : MonoBehaviour
+{
+
+  public int villagers = 5;
+  public int lumber = 0;
+  public int crops = 20;
+
+  public int GetValue(Type type)
+  {
+    switch (type)
+    {
+      case Type.VILLAGER:
+        {
+          return villagers;
+        }
+      case Type.LUMBER:
+        {
+          return lumber;
+        }
+      case Type.CROP:
+        {
+          return crops;
+        }
+    }
+    return 0;
+  }
+
+  public enum Type
+  {
+    VILLAGER,
+    LUMBER,
+    CROP
+  }
+
+}
