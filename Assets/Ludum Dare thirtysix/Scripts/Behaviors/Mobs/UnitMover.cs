a4f52b95b2c809455d5980a78832ed8c9d41b3b1
HitchH1k3r <hitch@hitchh1k3rsguide.com>
2016-08-28 01:35:32 -0400

    Animals Now Run Around In Circles!!!

--- Assets/Ludum Dare thirtysix/Scripts/Behaviors/Mobs/UnitMover.cs
+++ Assets/Ludum Dare thirtysix/Scripts/Behaviors/Mobs/UnitMover.cs
@@ -6,6 +6,7 @@ public class UnitMover : MonoBehaviour
 {
 
   public float moveTime = 1;
+  public float turnTime = 0.5f;
   public MoveStyle moveType;
 
   private Queue<IEnumerator> queuedMoves = new Queue<IEnumerator>();
@@ -17,6 +18,9 @@ public class UnitMover : MonoBehaviour
     if (queuedMoves.Count == 0)
     {
       MoveForward();
+      MoveForward();
+      MoveForward();
+      TurnLeft();
     }
 
     if (movement == null && queuedMoves.Count > 0)
@@ -27,23 +31,73 @@ public class UnitMover : MonoBehaviour
 
   public void MoveForward()
   {
-    queuedMoves.Enqueue(MoveTo(0 * 1.1f * (transform.rotation * Vector3.forward)));
+    queuedMoves.Enqueue(MoveTo(1.1f * Vector3.forward));
+  }
+
+  public void TurnLeft()
+  {
+    queuedMoves.Enqueue(TurnTo(-90));
+  }
+
+  private IEnumerator TurnTo(float deltaRotation)
+  {
+    Vector3 oldRot = transform.eulerAngles;
+    Vector3 oldPos = transform.position;
+    timer = 0;
+    while (timer < turnTime)
+    {
+      timer += Time.deltaTime;
+      float t = timer / turnTime;
+      float bounce = Ease.QuadPop(0, 2, t);
+      if (bounce > 1)
+      {
+        bounce = 2 - bounce;
+      }
+      if (moveType == MoveStyle.WADDLE)
+      {
+        bounce *= 0.1f;
+      }
+      else if (moveType == MoveStyle.STALK)
+      {
+        bounce *= 0.25f;
+      }
+      transform.position = oldPos + bounce * Vector3.up;
+      transform.rotation = Quaternion.Euler(oldRot.x, oldRot.y + Ease.QuadInOut(0, 1, t) * deltaRotation, oldRot.z);
+      yield return null;
+    }
+    transform.rotation = Quaternion.Euler(oldRot.x, oldRot.y + deltaRotation, oldRot.z);
+    transform.position = oldPos;
+    if (queuedMoves.Count > 0)
+    {
+      movement = StartCoroutine(queuedMoves.Dequeue());
+    }
+    else
+    {
+      movement = null;
+    }
   }
 
   private IEnumerator MoveTo(Vector3 deltaPos)
   {
     Vector3 oldRot = transform.eulerAngles;
     Vector3 oldPos = transform.position;
-    Vector3 target = oldPos + deltaPos;
+    Vector3 target = oldPos + transform.rotation * deltaPos;
     timer = 0;
     while (timer < moveTime)
     {
       timer += Time.deltaTime;
       float t = timer / moveTime;
-      float bounce = Ease.QuadPop(0, 2, t * 2);
-      if (t > 0.5)
+      float bounce = Ease.QuadPop(0.0f, 2.0f, t);
+      if (moveType == MoveStyle.WADDLE)
       {
-        bounce = Ease.QuadPop(0, 2, (t * 2) - 1);
+        if (t < 0.5f)
+        {
+          bounce = Ease.QuadPop(0.0f, 2.0f, t * 2);
+        }
+        else
+        {
+          bounce = Ease.QuadPop(0.0f, 2.0f, (t * 2) - 1);
+        }
       }
       if (bounce > 1)
       {
